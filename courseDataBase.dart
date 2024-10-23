dd96940ea6a4963ee967889432492950d072f272
cdbelala <cdbelala@gmail.com>
2024-10-23 11:32:22 -0500

    cleaned up depencies, all firebase prep work SHOULD (keyword SHOULD) be ready to go

--- courseDataBase.dart
+++ courseDataBase.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/widgets.dart';
+import 'dart:ui';
 
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+CollectionReference usersColl = firestore.collection('Courses');
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  final firestore = FirebaseFirestore.instance;
-  // ... rest of your app code
 }
